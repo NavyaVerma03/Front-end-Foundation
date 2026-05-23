7b8cc6e72f678ea9ff99089e8167b7b409158464
Navya Verma <navyaverma451@gmail.com>
2026-05-23 11:35:48 +0530

    Initial commit

---
+++ HTML+CSS+JS/8. Password Show and Hide/script.js
@@ -0,0 +1,22 @@
+let password = document.getElementById("password");
+
+let btn = document.getElementById("btn");
+
+btn.onclick = function(){
+
+    if(password.type == "password"){
+
+        password.type = "text";
+
+        btn.innerText = "Hide";
+
+    }
+    else{
+
+        password.type = "password";
+
+        btn.innerText = "Show";
+
+    }
+
+}
