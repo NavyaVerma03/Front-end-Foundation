3f49050284228924f1936c92c579ec1d6567d484
Navya Verma <navyaverma451@gmail.com>
2026-05-23 11:53:27 +0530

    Initial commit

---
+++ HTML+CSS+JS/10. Bulb ON OFF/script.js
@@ -0,0 +1,11 @@
+function turnOn(){
+
+  document.getElementById("bulb").src =
+  "https://www.w3schools.com/js/pic_bulbon.gif";
+}
+
+function turnOff(){
+
+  document.getElementById("bulb").src =
+  "https://www.w3schools.com/js/pic_bulboff.gif";
+}
